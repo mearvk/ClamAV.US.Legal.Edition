8aad96da94173f474e018870cb15221533da4811
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-30 11:15:22 -0400

    Add Herald 2 implementation

---
+++ herald/2/level.c
@@ -0,0 +1,4 @@
+/* Max Rupplin - MEARVK LLC - 2026. */
+#include "level.h"
+#include <stdio.h>
+int herald2_start(const char*p,const char*c){if(!p||!c)return -1;fprintf(stderr,"herald2 confirm: %s [%s]\n",p,c);return 0;}
